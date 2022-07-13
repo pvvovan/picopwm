339d7a31c59e247681ac2583b184f62073d8864a
PVVovan <pvvovan@gmail.com>
2022-07-13 19:49:20 +0200

    Blink by GP15 (pin 20)

--- hello_pwm.cpp
+++ hello_pwm.cpp
@@ -1,12 +1,15 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 
+// #define BLINK_PIN	PICO_DEFAULT_LED_PIN
+#define BLINK_PIN	15
+
 int main()
 {
 	// Tell the LED pin that the PWM is in charge of its value.
-	::gpio_set_function(PICO_DEFAULT_LED_PIN, GPIO_FUNC_PWM);
+	::gpio_set_function(BLINK_PIN, GPIO_FUNC_PWM);
 	// Figure out which slice we just connected to the LED pin
-	uint slice_num = ::pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN);
+	uint slice_num = ::pwm_gpio_to_slice_num(BLINK_PIN);
 
 	// Get some sensible defaults for the slice configuration. By default, the
 	// counter is allowed to wrap over its maximum range (0 to 2**16-1)
@@ -17,9 +20,9 @@ int main()
 	::pwm_init(slice_num, &config, true);
 
 	while (true) {
-		::pwm_set_gpio_level(PICO_DEFAULT_LED_PIN, 255 * 10);
+		::pwm_set_gpio_level(BLINK_PIN, 255 * 5);
 		sleep_ms(500);
-		::pwm_set_gpio_level(PICO_DEFAULT_LED_PIN, 0);
+		::pwm_set_gpio_level(BLINK_PIN, 0);
 		sleep_ms(500);
 	}
 }
