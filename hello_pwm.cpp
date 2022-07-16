65aca03e4d36841c09767934a7698c9a69709214
PVVovan <pvvovan@gmail.com>
2022-07-16 13:33:16 +0200

    Create WiFi access point

--- hello_pwm.cpp
+++ hello_pwm.cpp
@@ -2,15 +2,28 @@
 #include "hardware/pwm.h"
 #include "pico/cyw43_arch.h"
 
+#include "dhcpserver.h"
+
 // #define BLINK_PIN	PICO_DEFAULT_LED_PIN
 #define BLINK_PIN	15
 
 int main()
 {
+	stdio_init_all();
 	if (cyw43_arch_init()) {
 		printf("WiFi init failed");
 		return -1;
 	}
+	const char *ap_name = "picow_test";
+	const char *password = "password";
+	cyw43_arch_enable_ap_mode(ap_name, password, CYW43_AUTH_WPA2_AES_PSK);
+
+	// Start the dhcp server
+	ip4_addr_t gw, mask;
+	IP4_ADDR(&gw, 192, 168, 4, 1);
+	IP4_ADDR(&mask, 255, 255, 255, 0);
+	dhcp_server_t dhcp_server;
+	dhcp_server_init(&dhcp_server, &gw, &mask);
 
 	// Tell the LED pin that the PWM is in charge of its value.
 	::gpio_set_function(BLINK_PIN, GPIO_FUNC_PWM);
@@ -26,11 +39,22 @@ int main()
 	::pwm_init(slice_num, &config, true);
 
 	while (true) {
-		::pwm_set_gpio_level(BLINK_PIN, 255 * 5);
-		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-		sleep_ms(500);
-		::pwm_set_gpio_level(BLINK_PIN, 0);
-		cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-		sleep_ms(500);
+		static absolute_time_t led_time;
+		static bool led_on = true;
+
+		if (absolute_time_diff_us(get_absolute_time(), led_time) < 0) {
+			led_on = !led_on;
+			if (led_on) {
+				::pwm_set_gpio_level(BLINK_PIN, 255 * 5);
+				cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+			} else {
+				::pwm_set_gpio_level(BLINK_PIN, 0);
+				cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+			}
+			led_time = make_timeout_time_ms(500);
+		}
+
+		cyw43_arch_poll();
+		sleep_ms(1);
 	}
 }
